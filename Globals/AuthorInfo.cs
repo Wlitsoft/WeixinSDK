e81e5bab90e97847144cee8529aa3c2ae6a848b9
李亮 <admin@wlitsoft.com>
2016-12-18 21:21:57 +0800

    项目初始化。

---
+++ Globals/AuthorInfo.cs
@@ -0,0 +1,24 @@
+﻿/**************************************************************************************************
+ * 描述：
+ *      在此定义项目作者的信息。
+ * 
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyCompany("Wlitsoft")]
+[assembly: AssemblyCopyright("Copyright Wlitsoft 2012 - 2016")]
+[assembly: AssemblyTrademark("Copyright © Copyright")]
+[assembly: AssemblyProduct("Wlitsoft 框架")]
+
+#if DEBUG
+	[assembly: AssemblyConfiguration("DEBUG")]
+#else
+	[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguage("zh-CN")]
---
+++ src/WeixinSDK/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+#if DEBUG
+[assembly: AssemblyTitle("Wlitsoft.Framework.WeixinSDK 调试版")]
+#else
+[assembly: AssemblyTitle("Wlitsoft.Framework.WeixinSDK 发行版")]
+#endif
+[assembly: AssemblyDescription("Wlitsoft 框架 - 微信公众平台开发工具包")]
+[assembly: AssemblyCulture("")]
+
+//将 ComVisible 设置为 false 将使此程序集中的类型
+//对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("dc7af5f6-8d9f-46e6-bd49-3486426e4702")]
